821770ad1742a746b373d2bd0cf081f2eaebedd9
kox13 <woloszyndawid99@gmail.com>
2026-02-03 19:39:01 +0100

    style(agent): correct format

--- source/agent/src/config/config_loader.h
+++ source/agent/src/config/config_loader.h
@@ -13,32 +13,33 @@ namespace agent {
 namespace config {
 
 class ConfigLoader {
-  public:
-    static Config LoadConfig();
-    static Config LoadDefaultConfig();
+ public:
+  static Config LoadConfig();
+  static Config LoadDefaultConfig();
 
-  private:
-    ConfigLoader() = delete;
+ private:
+  ConfigLoader() = delete;
 
-    static void LoadConfigFile(Config &out_config);
-    static bool LoadUUID(Config &out_config);
-    static void CreateUUID(Config &out_config);
-    static void LoadCoreAffinity(toml::table &tbl, Config &out_config);
-    static void LoadInterval(toml::table &tbl, Config &out_config);
-    static void LoadServerAddress(toml::table &tbl, Config &out_config);
-    static void LoadServerPort(toml::table &tbl, Config &out_config);
-    static void LoadCollectors(toml::table &tbl, Config &out_config);
-    static void CheckKeys(toml::table &tbl);
+  static void LoadConfigFile(Config& out_config);
+  static bool LoadUUID(Config& out_config);
+  static void CreateUUID(Config& out_config);
+  static void LoadCoreAffinity(toml::table& tbl, Config& out_config);
+  static void LoadInterval(toml::table& tbl, Config& out_config);
+  static void LoadServerAddress(toml::table& tbl, Config& out_config);
+  static void LoadServerPort(toml::table& tbl, Config& out_config);
+  static void LoadCollectors(toml::table& tbl, Config& out_config);
+  static void CheckKeys(toml::table& tbl);
 
-    static std::filesystem::path kConfigFile;
-    static std::filesystem::path kUUIDFile;
-    static std::set<std::string_view, std::less<>> kValidTopLevelKeys;
-    static std::map<std::string_view, std::set<std::string_view, std::less<>>, std::less<>>
-        kValidCollectors;
+  static std::filesystem::path kConfigFile;
+  static std::filesystem::path kUUIDFile;
+  static std::set<std::string_view, std::less<>> kValidTopLevelKeys;
+  static std::map<std::string_view, std::set<std::string_view, std::less<>>,
+                  std::less<>>
+      kValidCollectors;
 };
 
-} // namespace config
-} // namespace agent
-} // namespace volta
+}  // namespace config
+}  // namespace agent
+}  // namespace volta
 
-#endif // VOLTA_AGENT_CONFIG_CONFIG_LOADER_H_
+#endif  // VOLTA_AGENT_CONFIG_CONFIG_LOADER_H_
