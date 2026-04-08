0094bcec07eb32e6e437167f6d0f53ab14fe5429
Filip <fil.wolinski@gmail.com>
2026-04-08 16:01:21 +0200

    fix: MSR read exception condition

--- source/agent/src/collectors/rapl_collector.cc
+++ source/agent/src/collectors/rapl_collector.cc
@@ -1,9 +1,9 @@
 #include "rapl_collector.h"
 
-#include <fcntl.h>
-#include <unistd.h>
-#include <filesystem>
 #include <algorithm>
+#include <fcntl.h>
+#include <filesystem>
+#include <unistd.h>
 
 #include <chrono>
 #include <cmath>
@@ -13,11 +13,7 @@ namespace agent {
 namespace collectors {
 
 RaplCollector::RaplCollector() {
-  try {
-    OpenMSR();
-  }catch(std::exception e){
-    throw e;
-  }
+  OpenMSR();
   uint64_t readout = ReadMSR(0, MSR_RAPL::POWER_UNIT);
   power_units_ = pow(0.5, (double)(readout & 0xf));
   energy_units_ = pow(0.5, (double)((readout >> 8) & 0x1f));
@@ -31,14 +27,12 @@ std::vector<Metric> RaplCollector::Collect() {
 
   try {
     readout = ReadMSR(0, MSR_RAPL::PKG::ENERGY_STATUS);
-  }
-  catch(MSR_Read_Exception){
+  } catch (MSR_Read_Exception) {
     return {};
   }
 
   double value = energy_units_ * readout;
 
-  // TODO add more metrics
   Metric m;
   m.name = "cpu_energy_usage_total";
   m.value = value - last_value;
@@ -51,7 +45,7 @@ std::vector<Metric> RaplCollector::Collect() {
 
 uint64_t RaplCollector::ReadMSR(uint8_t core, uint32_t offset) {
   uint64_t data;
-  if(core > MSR_files_.size()){
+  if (core + 1 > MSR_files_.size()) {
     throw MSR_Read_Exception();
   }
   // c-like read for thread safety
@@ -63,32 +57,36 @@ uint64_t RaplCollector::ReadMSR(uint8_t core, uint32_t offset) {
 }
 
 void RaplCollector::OpenMSR() {
-    const std::filesystem::path cpu_base = "/dev/cpu";
-    MSR_files_ = std::vector<int>();
-    std::error_code ec;
-    if (!std::filesystem::exists(cpu_base, ec)) {
-        throw MSR_Open_Exception();
-    }
-    for (const auto& entry : std::filesystem::directory_iterator(cpu_base)) {
-        if (!entry.is_directory()) continue;
+  const std::filesystem::path cpu_base = "/dev/cpu";
+  MSR_files_ = std::vector<int>();
+  std::error_code ec;
 
-        const auto& dirname = entry.path().filename().string();
-        if (!std::ranges::all_of(dirname, ::isdigit)) continue;
+  if (!std::filesystem::exists(cpu_base, ec)) {
+    throw MSR_Open_Exception();
+  }
 
-        int fd = open((entry.path() / "msr").c_str(), O_RDONLY);
-        if (fd >= 0) {
-            MSR_files_.push_back(fd);
-        }
+  for (const auto &entry : std::filesystem::directory_iterator(cpu_base)) {
+    if (!entry.is_directory())
+      continue;
+
+    const auto &dirname = entry.path().filename().string();
+    if (!std::ranges::all_of(dirname, ::isdigit))
+      continue;
+
+    int fd = open((entry.path() / "msr").c_str(), O_RDONLY);
+    if (fd >= 0) {
+      MSR_files_.push_back(fd);
     }
+  }
 }
 
 void RaplCollector::CloseMSR(int fd) { close(fd); }
 
-RaplCollector::~RaplCollector(){
-  for(auto file : MSR_files_){
+RaplCollector::~RaplCollector() {
+  for (auto file : MSR_files_) {
     CloseMSR(file);
   }
 };
-}  // namespace collectors
-}  // namespace agent
-}  // namespace volta
+} // namespace collectors
+} // namespace agent
+} // namespace volta
